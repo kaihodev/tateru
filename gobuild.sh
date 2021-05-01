d62296702995ea55029cdcade1d7443f10d2d997
@kaihodev <kaihodev@gmail.com>
2021-05-01 17:35:32 -0400

    refactor: modify gobuild to shebang bash

--- gobuild.sh
+++ gobuild.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 TARGET_OSES=(darwin dragonfly freebsd freebsd freebsd linux linux linux linux linux linux linux linux linux linux netbsd netbsd netbsd openbsd openbsd openbsd solaris windows windows)
 
 ARCHES=(amd64 amd64 386 amd64 arm 386 amd64 arm arm64 ppc64 ppc64le mips mipsle mips64 mips64le 386 amd64 arm 386 amd64 arm amd64 386 amd64)
@@ -20,7 +22,7 @@ for ((i = 0; i < ${#TARGET_OSES[@]}; ++i )); do
   NUM=$((i + 1));
 
   echo "[$NUM/${#TARGET_OSES[@]}] building $GOOS/$GOARCH";
-  go build -o build/$GOOS-$GOARCH-tateru & pids+=("$!") &
+  go build -o build/"$GOOS"-"$GOARCH"-tateru & pids+=("$!") &
 done
 
 wait
