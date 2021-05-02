3fcacd9fa82fffee93bf0f9f60e5f67d93fa77fb
@kaihodev <kaihodev@gmail.com>
2021-05-01 20:46:21 -0400

    fix: experimental force build

--- gobuild.sh
+++ gobuild.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-TARGET_OSES=(darwin dragonfly freebsd freebsd freebsd linux linux linux linux linux linux linux linux linux linux netbsd netbsd netbsd openbsd openbsd openbsd solaris windows windows)
+TARGET_OSES=(darwin) # dragonfly freebsd freebsd freebsd linux linux linux linux linux linux linux linux linux linux netbsd netbsd netbsd openbsd openbsd openbsd solaris windows windows)
 
-ARCHES=(amd64 amd64 386 amd64 arm 386 amd64 arm arm64 ppc64 ppc64le mips mipsle mips64 mips64le 386 amd64 arm 386 amd64 arm amd64 386 amd64)
+ARCHES=(amd64) # amd64 386 amd64 arm 386 amd64 arm arm64 ppc64 ppc64le mips mipsle mips64 mips64le 386 amd64 arm 386 amd64 arm amd64 386 amd64)
 
 pids=( )
 
