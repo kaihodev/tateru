787368b69579976eadce046fdf7076e31c6f98b8
Red Panther <41539361+PantheraRed@users.noreply.github.com>
2021-06-14 12:08:21 +0530

    Fix grammar in error message

--- npm/tateru.js
+++ npm/tateru.js
@@ -1,2 +1,2 @@
 #!/usr/bin/env node
-throw Error('Dame dayo onii-chan!~ if you see this, install failed ｡ﾟヽ(ﾟ´Д｀)ﾉﾟ｡: ｡･ﾟ');
+throw Error('Dame dayo onii-chan!~ if you see this, installation failed ｡ﾟヽ(ﾟ´Д｀)ﾉﾟ｡: ｡･ﾟ');
